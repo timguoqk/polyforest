4c32adae935a469a04daa177ddb6c536a9d99854
Tim Guo <timguoqk@users.noreply.github.com>
2015-05-23 16:50:23 -0700

    music frequency changes lights[0].diffuse (proof of concept)

--- project.js
+++ project.js
@@ -1,31 +1,29 @@
 var canvas;
 var gl;
 var groundSize, ground, groundBuffer;
-var geoNumber, geo, geoBuffer;
-var normals, normalBuffer;
-var geo = [];
-var normals = [];
-var projection, camera;
-var inv_projection;
+var geoNumber, geo = [], geoBuffer;
+var normals = [], normalBuffer;
+var projection, inv_projection;
 var locations = [];  //locations of geometries
 var time_old = 0;
-var _camera, _vPosition, _projection, _modelView, _normal; //handles
+var _vPosition, _projection, _modelView, _normal; //handles
 var key = {left: false, right: false, up: false, down: false};
+var analyser, frequency;
 
 var vertices = [
-                vec3(-0.5, 0.0, 0.0),
-                vec3(0.0, 0.0, 0.5),
-                vec3(0.5, 0.0, 0.0), 
-                vec3(0.0, 20.0, 0.0),
-                vec3(-0.1, 17.0, 0.0),
-                vec3(0.0, 17.0, 0.1),
-                vec3(0.0, 16.0, 0.0),
-                vec3(-1.7, 21.0, 1.4)
-    ];
+    vec3(-0.5, 0.0, 0.0),
+    vec3(0.0, 0.0, 0.5),
+    vec3(0.5, 0.0, 0.0), 
+    vec3(0.0, 20.0, 0.0),
+    vec3(-0.1, 17.0, 0.0),
+    vec3(0.0, 17.0, 0.1),
+    vec3(0.0, 16.0, 0.0),
+    vec3(-1.7, 21.0, 1.4)
+];
 
 var lights = [{
     position: vec4(1.0, 1.0, 1.0, 0.0),
-    ambient: vec4(1.0, 1.0, 1.0, 0.1),
+    ambient: vec4(1.0, 1.0, 1.0, 0.2),
     // diffuse: vec4(1.0, 1.0, 1.0, 0.2),
     diffuse: vec4(1.0, 1.0, 1.0, 0.0),
     specular: vec4(0.0, 0.0, 0.0, 0.0),
@@ -52,7 +50,8 @@ window.ondeviceorientation = gyroscopeHandler;
 window.onload = function() {
     var canvas = document.getElementById("gl-canvas");
     gl = WebGLUtils.setupWebGL(canvas);
-    if (!gl) { alert("WebGL isn't available"); }
+    if (!gl)
+        alert("WebGL isn't available");
     gl.viewport(0, 0, canvas.width, canvas.height);
     
     // Set clear color to be black
@@ -66,9 +65,7 @@ window.onload = function() {
     // Load shaders and initialize attribute buffers
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
-    
-    drawTree(vertices[0], vertices[1], vertices[2], vertices[3]);
-    drawTree(vertices[4], vertices[5], vertices[6], vertices[7]);
+
     // Get handles
     _vPosition = gl.getAttribLocation(program, "vPosition");
     _projection = gl.getUniformLocation(program, "projection");
@@ -83,6 +80,8 @@ window.onload = function() {
     
     initialSetup();
     
+    drawTree(vertices[0], vertices[1], vertices[2], vertices[3]);
+    drawTree(vertices[4], vertices[5], vertices[6], vertices[7]);
     for (var i = 0; i < geoNumber; i++) {
         var x = (Math.random() -0.5) * groundSize;
         var y = 0.0;
@@ -98,6 +97,15 @@ window.onload = function() {
     gl.enableVertexAttribArray(_vPosition);
     gl.enableVertexAttribArray(_normal);
     
+    // Set up audio
+    var ctx = new AudioContext();
+    var audio = document.getElementById('bgm');
+    var audioSrc = ctx.createMediaElementSource(audio);
+    analyser = ctx.createAnalyser();  // This is global
+    audioSrc.connect(analyser);
+    audioSrc.connect(ctx.destination);
+    audio.play();
+
     animate(0);
 };
 
@@ -129,6 +137,7 @@ function animate(time) {
         else if (key.right)
             locations[i] = mult(rotate(0.02 * dt, vec3(0.0, 1.0, 0.0)),locations[i]);
     }
+    analyzeAudio();
     render();
     window.requestAnimationFrame(animate);
 }
@@ -235,6 +244,24 @@ function render() {
     }
 }
 
+function analyzeAudio() {
+    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(frequencyData);
+
+    frequency = [];
+    for (var i = 0; i < 5; i ++) {
+        frequency.push(0);
+        for (var j = 0; j < 200; j ++)
+            frequency[i] += frequencyData[10*i + j];
+    }
+    // Push the sum to the array
+    frequency.push(frequency.reduce(function(previousValue, currentValue) {
+        return previousValue + currentValue;
+    }));
+
+    // Apply frequency to lights
+    lights[0].diffuse = vec4(frequency[4]/20000, frequency[4]/15000, frequency[4]/210000, 1.0);
+}
 
 function drawTree(a, b, c, d) {
     geo.push(a);
@@ -255,8 +282,6 @@ function drawTree(a, b, c, d) {
     normals.push(c[0],c[1], c[2], 0.0);
     geo.push(d);
     normals.push(d[0],d[1], d[2], 0.0);
-
-    
 }
 
 /********  Interface  ********/
@@ -361,7 +386,6 @@ function inverse4(m) {
 
 function find_clip_coord(location, offset) {
     var pos1 = times(location, vec4(offset, 0.0, - offset, 1.0));
-    //pos = times(camera, pos);
     pos1 = times(projection, pos1);
     var pos2 = times(location, vec4(- offset, 0.0, - offset, 1.0));
     pos2 = times(projection, pos2);
@@ -373,4 +397,10 @@ function find_clip_coord(location, offset) {
     pos.push(Math.max(Math.abs(pos1[3]), Math.abs(pos2[3])));    
 
     return pos;
+}
+
+// From http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+function shadeColor1(color, percent) {  
+    var num = parseInt(color.slice(1),16), amt = Math.round(2.55 * percent), R = (num >> 16) + amt, G = (num >> 8 & 0x00FF) + amt, B = (num & 0x0000FF) + amt;
+    return "#" + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1);
 }
